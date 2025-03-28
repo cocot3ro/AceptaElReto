580b8f21900baa97de14348a7a1f1620ce2db0db
cocot3ro <pablooterocastro15@gmail.com>
2025-03-28 16:46:37 +0100

    Aer106: Accepted

--- 100-199/Aer106/src/App.java
+++ 100-199/Aer106/src/App.java
@@ -4,10 +4,22 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for (String codigo; !(codigo = sc.next()).equals("0"); ) {
+        for (String codigo = sc.next(); !codigo.equals("0"); codigo = sc.next()) {
             if (checkEan(codigo)) {
                 System.out.print("SI");
-                if (codigo.length() > 8) {
+                int length = codigo.length();
+
+                if (length > 8) {
+
+                    StringBuilder sb = new StringBuilder();
+
+                    if (length < 13) {
+                        for (int i = 13 - length; i > 0; i--) {
+                            sb.append("0");
+                        }
+                        codigo = sb + codigo;
+                    }
+
                     if (codigo.startsWith("380")) {
                         System.out.println(" Bulgaria");
                     } else if (codigo.startsWith("50")) {
@@ -40,8 +52,9 @@ public class App {
 
     private static boolean checkEan(String codigo) {
         int sum = 0;
-        for (int i = 1; i < codigo.length(); i++) {
-            int digit = Integer.parseInt(codigo.charAt(codigo.length() - 1 - i) + "");
+        int l = codigo.length();
+        for (int i = 1; i < l; i++) {
+            int digit = codigo.charAt(l - 1 - i) - '0';
             if (i % 2 == 1) {
                 sum += (digit * 3);
             } else {
@@ -49,6 +62,6 @@ public class App {
             }
         }
 
-        return (sum + Integer.parseInt(codigo.charAt(codigo.length() - 1) + "")) % 10 == 0;
+        return (sum + codigo.charAt(l - 1) - '0') % 10 == 0;
     }
 }
