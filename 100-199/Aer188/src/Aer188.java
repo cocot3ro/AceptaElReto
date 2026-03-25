89d3dbc0ecb4858a735c3fd85823372325becce9
cocot3ro <pablooterocastro15@gmail.com>
2026-03-25 17:37:31 +0100

    Aer188: Accepted

--- 100-199/Aer188/src/Aer188.java
+++ 100-199/Aer188/src/Aer188.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Aer188 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            if (Solution.solve(sc.nextLine().split(" "))) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
+class Solution {
+    public static boolean solve(String[] s) {
+        for (int i = 1; i < s.length; i++) {
+            int prevLength = s[i - 1].length();
+            String prev = s[i - 1].substring(prevLength - 2, prevLength);
+            String curr = s[i].substring(0, 2);
+
+            if (!prev.equals(curr)) return false;
+        }
+
+        return true;
+    }
 }
